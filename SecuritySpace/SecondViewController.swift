f2d5ed46f941d5041351b7d6f7ec91df4e6e54dd
lin <leo0102@hotmail.com>
2017-01-04 23:09:22 +0800

    Initial Commit

---
+++ SecuritySpace/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SecuritySpace
+//
+//  Created by Lin on 17/1/4.
+//  Copyright © 2017年 leo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
